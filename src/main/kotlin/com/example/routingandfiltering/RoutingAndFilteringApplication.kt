f84609b8e669d3b4e393c034a11b95d1eec2b997
mattbradburyr3 <matthew.bradbury@r3.com>
2020-12-02 13:07:11 +0000

    add comment

--- src/main/kotlin/com/example/routingandfiltering/RoutingAndFilteringApplication.kt
+++ src/main/kotlin/com/example/routingandfiltering/RoutingAndFilteringApplication.kt
@@ -6,6 +6,9 @@ import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 import org.springframework.context.annotation.Bean
 
+// Follows Routing and Filtering tutorial https://spring.io/guides/gs/routing-and-filtering/ but converts to kotlin + adds a second 'cars' server
+// also requires the books and cars repos
+
 @EnableZuulProxy
 @SpringBootApplication
 class RoutingAndFilteringApplication{
